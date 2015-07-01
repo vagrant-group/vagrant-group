66ecafa02fee7dedbf1e7296adbf727eef69419b
Krzysztof Magosa <krzysztof@magosa.pl>
2015-07-01 18:51:45 +0200

    remove unused auto-generated files

--- lib/vagrant/vm/group.rb
+++
@@ -1,9 +0,0 @@
-require "vagrant/vm/group/version"
-
-module Vagrant
-  module Vm
-    module Group
-      # Your code goes here...
-    end
-  end
-end
--- lib/vagrant/vm/group/version.rb
+++
@@ -1,7 +0,0 @@
-module Vagrant
-  module Vm
-    module Group
-      VERSION = "0.0.1"
-    end
-  end
-end
